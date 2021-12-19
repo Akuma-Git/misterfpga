22420f506f657bc867ff4fb8290a7696e7dc0688
Akuma-Git <70658206+Akuma-Git@users.noreply.github.com>
2021-12-19 10:54:39 +0000

    Update unstable-update_playstation-nightlies.sh

--- unstable-update_playstation-nightlies.sh
+++ unstable-update_playstation-nightlies.sh
@@ -1,3 +1,67 @@
 #!/bin/sh
 version=0.04
-echo version=$version
+#
+#  MiSTer-unstable-nightlies Updater (c) 2021 by Akuma GPLv2
+#
+#  20211219 update: added self-update
+#  20211219 update: moved maxkeep=N into self.ini
+#  20211218 update: added maxkeep=N, keep max N nightlies
+#  20211213 update: using new source, json file from @theypsilon
+#  20211212 update: corrected bios download directory
+#
+corename="PlayStation"
+self="$(readlink -f "$0")"
+
+conf="${self%.*}.ini"
+[ -f "$conf" ] && . "$conf"
+
+trap "result" 0 1 3 15
+
+result(){
+  case "$?" in
+    0) echo "core version: ${url##*/}";;
+    1) echo "self: updated self";;
+  100) echo "error: cannot reach url";;
+  101) echo "error: cannot write to sdcard";;
+  102) echo "error: download failed";;
+  103) echo "error: checksum failed";;
+  104) echo "error: json parsing failed";;
+  esac
+}
+
+makedir(){ [ -d "$1" ] || { mkdir -p "$1" || exit 101;};}
+download(){ wget --no-cache -q "$2" -O "$1" || { rm "$1";exit 102;};}
+checksum(){ md5sum "$1"|grep -q "$2" || { rm "$1";exit 103;};}
+
+selfurl="https://raw.githubusercontent.com/Akuma-Git/misterfpga/main/unstable-update_playstation-nightlies.sh"
+selfurl_version="$(download "$selfurl"|sed -n 's,^version=,,;2p')"
+
+[ "$selfurl_version" = "$version" ] || {
+  tempfile="$(mktemp -u)"; download "$tempfile" "$selfurl"
+  mv "$tempfile" "$self";chmod +x "$self";exec "$self"; exit 1
+}
+
+json="https://raw.githubusercontent.com/MiSTer-unstable-nightlies/Unstable_Folder_MiSTer/main/db_unstable_nightlies_folder.json"
+updates="$(wget -q "$json" -O -)" || exit 100
+
+coredir="/media/fat/_Unstable";makedir "$coredir"
+gamesdir="/media/fat/games/${corename}";makedir "$gamesdir"
+
+biosurl="https://raw.githubusercontent.com/archtaurus/RetroPieBIOS/master/BIOS/scph1001.bin"
+bioshash="924e392ed05558ffdb115408c263dccf"
+
+biosfile="$gamesdir/boot.rom"
+[ -f "$biosfile" ] || download "$biosfile" "$biosurl"
+[ -n "$bioshash" ] && checksum "$biosfile" "$bioshash"
+
+export $(echo $updates|grep -o "\"_Unstable/${corename}.[^}]*}"|sed 's,^.*{,,;s,},,;s,": ,=,g;s/,/\n/g;s,",,g')
+[ -n "$url" -o -n "$hash" ] || exit 104
+
+corefile="$coredir/${url##*/}"
+[ -f "$corefile" ] || download "$corefile" "$url"
+[ -f "$corefile" ] || checksum "$corefile" "$hash"
+
+[ -n "$maxkeep" -a -n "$coredir" -a -n "$corename" ] \
+  && { ls -t "${coredir}/${corename}_unstable_"*".rbf"|awk "NR>$maxkeep"|xargs -r rm;}
+
+exit 0
