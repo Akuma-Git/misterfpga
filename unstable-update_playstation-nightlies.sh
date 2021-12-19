975e16f88f4f49cc24ff3c0c3918c74c39e3ad3d
Akuma-Git <70658206+Akuma-Git@users.noreply.github.com>
2021-12-19 14:43:05 +0000

    Update unstable-update_playstation-nightlies.sh

--- unstable-update_playstation-nightlies.sh
+++ unstable-update_playstation-nightlies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=0.04
+version=0.05
 #
 #  MiSTer-unstable-nightlies Updater (c) 2021 by Akuma GPLv2
 #
@@ -10,6 +10,8 @@ version=0.04
 #  20211212 update: corrected bios download directory
 #
 corename="PlayStation"
+trigger="PSX"
+
 self="$(readlink -f "$0")"
 
 conf="${self%.*}.ini"
@@ -19,7 +21,7 @@ trap "result" 0 1 3 15
 
 result(){
   case "$?" in
-    0) echo "core version: ${url##*/}";;
+    0) echo "core version: ${corefile##*/}";;
     1) echo "self: updated self";;
   100) echo "error: cannot reach url";;
   101) echo "error: cannot write to sdcard";;
@@ -31,18 +33,16 @@ result(){
 
 makedir(){ [ -d "$1" ] || { mkdir -p "$1" || exit 101;};}
 download(){ wget --no-cache -q "$2" -O "$1" || { rm "$1";exit 102;};}
+urlcat(){ wget --no-cache -q "$1" -O -;}
 checksum(){ md5sum "$1"|grep -q "$2" || { rm "$1";exit 103;};}
 
 selfurl="https://raw.githubusercontent.com/Akuma-Git/misterfpga/main/unstable-update_playstation-nightlies.sh"
-selfurl_version="$(wget --no-cache -q "$selfurl" -O -|sed -n 's,^version=,,;2p')"
+selfurl_version="$(urlcat "$selfurl"|sed -n 's,^version=,,;2p')"
 
 [ "$selfurl_version" = "$version" ] || {
   tempfile="$(mktemp -u)"; download "$tempfile" "$selfurl"
   mv "$tempfile" "$self";chmod +x "$self";exec "$self"; exit 1
 }
-
-json="https://raw.githubusercontent.com/MiSTer-unstable-nightlies/Unstable_Folder_MiSTer/main/db_unstable_nightlies_folder.json"
-updates="$(wget --no-cache -q "$json" -O -)" || exit 100
 
 coredir="/media/fat/_Unstable";makedir "$coredir"
 gamesdir="/media/fat/games/${corename}";makedir "$gamesdir"
@@ -54,10 +54,12 @@ biosfile="$gamesdir/boot.rom"
 [ -f "$biosfile" ] || download "$biosfile" "$biosurl"
 [ -n "$bioshash" ] && checksum "$biosfile" "$bioshash"
 
-export $(echo $updates|grep -o "\"_Unstable/${corename}.[^}]*}"|sed 's,^.*{,,;s,},,;s,": ,=,g;s/,/\n/g;s,",,g')
+nightliesurl="https://raw.githubusercontent.com/MiSTer-unstable-nightlies/Unstable_Folder_MiSTer/main/db_unstable_nightlies_folder.json"
+nightlies="$(urlcat "$nightliesurl")" || exit 100
+export $(echo $nightlies|grep -o "\"_Unstable/${trigger}.[^}]*}"|sed 's,^.*{,,;s,},,;s,": ,=,g;s/,/\n/g;s,",,g')
 [ -n "$url" -o -n "$hash" ] || exit 104
 
-corefile="$coredir/${url##*/}"
+corefile="$coredir/${url##*/}";corefile="${corefile/PSX/PlayStation}"
 [ -f "$corefile" ] || download "$corefile" "$url"
 [ -f "$corefile" ] || checksum "$corefile" "$hash"
 
