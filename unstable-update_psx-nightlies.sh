08c18337f3c32594fde617a5cc91fc8b2882c991
Akuma-Git <70658206+Akuma-Git@users.noreply.github.com>
2021-12-21 20:13:31 +0000

    Update unstable-update_psx-nightlies.sh

--- unstable-update_psx-nightlies.sh
+++ unstable-update_psx-nightlies.sh
@@ -3,6 +3,9 @@ version=0.06
 #
 #  MiSTer-unstable-nightlies Updater (c) 2021 by Akuma GPLv2
 #
+#  20211221 update: added auto-rename if old PlayStation games folder is found
+#  20211221 update: added one-time run for unstable-update_main-nightlies.sh
+#  20211221 update: added exit to urlcat as extra safety measure
 #  20211219 update: fixed upstream rename of PlayStation core to PSX
 #  20211219 update: added self-update
 #  20211219 update: moved maxkeep=N into self.ini
@@ -11,6 +14,7 @@ version=0.06
 #  20211212 update: corrected bios download directory
 #
 corename="PSX"
+oldcorename="PlayStation"
 
 self="$(readlink -f "$0")"
 
@@ -21,7 +25,7 @@ trap "result" 0 1 3 15
 
 result(){
   case "$?" in
-    0) echo "core version: ${corefile##*/}";;
+    0) echo -e "core version: ${corefile##*/}\n";;
     1) echo "self: updated self";;
   100) echo "error: cannot reach url";;
   101) echo "error: cannot write to sdcard";;
@@ -33,7 +37,7 @@ result(){
 
 makedir(){ [ -d "$1" ] || { mkdir -p "$1" || exit 101;};}
 download(){ wget --no-cache -q "$2" -O "$1" || { rm "$1";exit 102;};}
-urlcat(){ wget --no-cache -q "$1" -O -;}
+urlcat(){ wget --no-cache -q "$1" -O - || exit 100;}
 checksum(){ md5sum "$1"|grep -q "$2" || { rm "$1";exit 103;};}
 
 selfurl="https://raw.githubusercontent.com/Akuma-Git/misterfpga/main/unstable-update_psx-nightlies.sh"
@@ -45,12 +49,14 @@ selfurl_version="$(urlcat "$selfurl"|sed -n 's,^version=,,;2p')"
 }
 
 coredir="/media/fat/_Unstable";makedir "$coredir"
-gamesdir="/media/fat/games/${corename}";makedir "$gamesdir"
+gamesdir="/media/fat/games"
+psxdir="$gamesdir/${corename}";makedir "$psxdir"
+
 
 biosurl="https://raw.githubusercontent.com/archtaurus/RetroPieBIOS/master/BIOS/scph1001.bin"
 bioshash="924e392ed05558ffdb115408c263dccf"
 
-biosfile="$gamesdir/boot.rom"
+biosfile="$psxdir/boot.rom"
 [ -f "$biosfile" ] || download "$biosfile" "$biosurl"
 [ -n "$bioshash" ] && checksum "$biosfile" "$bioshash"
 
@@ -62,6 +68,20 @@ export $(echo $nightlies|grep -o "\"_Unstable/${corename}.[^}]*}"|sed 's,^.*{,,;
 corefile="$coredir/${url##*/}"
 [ -f "$corefile" ] || download "$corefile" "$url"
 [ -f "$corefile" ] || checksum "$corefile" "$hash"
+
+[ -d "${gamesdir}/${oldcorename}" ] && {
+  echo "NOTICE: renaming directories with new core name:"
+  find "/media/fat" -maxdepth 2 -type d -name "$oldcorename" -exec rename -v $oldcorename $corename {} \;
+}
+
+scripturl="https://raw.githubusercontent.com/Akuma-Git/misterfpga/main/unstable-update_main-nightlies.sh"
+scriptfile="/media/fat/Scripts/${scripturl##*/}"
+[ -f "$scriptfile" ] || {
+  download "$scriptfile" "$scripturl"
+  misterhash="2663b54d09ddbfa248360cd29501b5e1"
+  misterfile="/media/fat/MiSTer"
+  md5sum "$misterfile"|grep -q "$misterhash" && exec "$scriptfile" #one-time-exec
+}
 
 [ -n "$maxkeep" -a -n "$coredir" -a -n "$corename" ] \
   && { ls -t "${coredir}/${corename}_unstable_"*".rbf"|awk "NR>$maxkeep"|xargs -r rm;}
