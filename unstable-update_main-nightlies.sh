04178c24f19094341fce101e64c4b040df3571aa
Akuma-Git <70658206+Akuma-Git@users.noreply.github.com>
2021-12-21 17:49:24 +0000

    Create unstable-update_main-nightlies.sh

---
+++ unstable-update_main-nightlies.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+version=0.00
+#
+#  MiSTer-unstable-nightlies Updater (c) 2021 by Akuma GPLv2
+#
+#  20211220 initial release
+#
+self="$(readlink -f "$0")"
+
+trap "result" 0 1 3 15
+
+result(){
+  case "$?" in
+    0) echo "MiSTer version: ${tempfile##*/}";;
+   99) echo "self: updated self";;
+  100) echo "error: cannot reach url";;
+  101) echo "error: cannot write to sdcard";;
+  102) echo "error: download failed";;
+  103) echo "error: checksum failed";;
+  104) echo "MiSTer version: already up to date";;
+  esac
+}
+
+makedir(){ [ -d "$1" ] || { mkdir -p "$1" || exit 101;};}
+download(){ wget --no-cache -q "$2" -O "$1" || { rm "$1";exit 102;};}
+urlcat(){ wget --no-cache -q "$1" -O - || exit 100;}
+checksum(){ md5sum "$1"|grep -q "$2" || { rm "$1";exit 103;};}
+
+selfurl="https://raw.githubusercontent.com/Akuma-Git/misterfpga/main/unstable-update_main-nightlies.sh"
+selfurl_version="$(urlcat "$selfurl"|sed -n 's,^version=,,;2p')"
+
+[ "$selfurl_version" = "$version" ] || {
+  tempfile="$(mktemp -u)"; download "$tempfile" "$selfurl"
+  mv "$tempfile" "$self";chmod +x "$self";exec "$self"; exit 99
+}
+
+nightliesurl="https://github.com/MiSTer-unstable-nightlies/Main_MiSTer/releases/tag/unstable-builds"
+nightliesfile="$(wget -q $misterurl -O -|grep -o "MiSTer_unstable_*"|tail -1)"
+[ -n "$nightliesfile" ] || exit 101
+
+nightliesurl="https://github.com/MiSTer-unstable-nightlies/${nightliesfile}"
+nightliessize="$(wget --spider "$nightliesurl" 2>&1|grep ^Length|cut -d' ' -f2)"
+[ -n "$tempsize" ] || exit 101
+
+misterfile="/media/fat/MiSTer"
+mistersize="$(stat -c%s "$misterfile")"
+[ "$nightliessize" = "$mistersize" ] && exit 104
+
+tempfile="$(mktemp -u)"
+download "$tempfile" "$nightliesurl"
+tempsize="$(stat -c%s "$tempfile")"
+[ "$nightliessize" = "$tempsize" ] || exit 102
+
+mv -f "${misterfile}" "${misterfile}.bak"
+mv -f "${tempfile}" "$misterfile"
+chmod +x "$misterfile"
+
+exit 0
